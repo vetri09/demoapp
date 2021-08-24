f534042ab0b049e44b3daec19a384c75783f483a
Vetrivelan <vetri0907.vv@gmail.com>
2021-08-24 18:23:54 +0530

    second commit

---
+++ backend/models/user.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose')
+const userSchema = new mongoose.Schema({
+    name:String,
+    email:String,
+    phone:String
+})
+const user = mongoose.model('users',userSchema)
+module.exports = user
--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var router = express.Router();
+const user = require('../models/user')
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
-
+router.get('/', async(req,res)=>{
+  const list = await user.find()
+  res.send({
+    list,
+    message:"users list"
+  })
+})
+// POST user
+router.post('/register', async(req,res)=>{
+  await new user(req.body).save()
+  res.send({
+    message:"user added"
+  })
+})
 module.exports = router;
